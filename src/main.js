2168500ee2ba51010c50a2120c63ae1938db863c
Joe_Zhang <cjt3399@gmail.com>
2024-03-05 23:13:00 +0800

    製作一個筆記本網頁

---
+++ src/main.js
@@ -0,0 +1,9 @@
+const main = document.querySelector(".MC");
+
+let aCode = `<h2>12345</h2>
+    <pre>
+        <code>console.log(":("))</code>
+    </pre>
+`;
+
+main.insertAdjacentHTML("beforeend", aCode);
