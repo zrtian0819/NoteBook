07a9c1baf4d85aec1734ddb24c100fb01512d249
Joe_Zhang <cjt3399@gmail.com>
2024-03-10 18:19:22 +0800

    抓取JSON功能實現

--- src/main.js
+++ src/main.js
@@ -1,9 +1,23 @@
 const main = document.querySelector(".MC");
 
-let aCode = `<h2>12345</h2>
-    <pre>
-        <code>console.log(":("))</code>
-    </pre>
-`;
+// 異步函數的使用
+async function getJson() {
+  try {
+    const Obj = await fetch("./123.json");
+    const MyJson = await Obj.json();
+    return MyJson;
+  } catch (e) {
+    console.log(e);
+  }
+}
 
-main.insertAdjacentHTML("beforeend", aCode);
+let MyObj = getJson();
+
+MyObj.then(function (data) {
+  // 把連結顯示於頁面
+
+  console.log(data);
+  console.log(data[0].contents);
+  main.innerHTML = data[0].contents;
+  // main.textContent = data[0].contents;
+});
