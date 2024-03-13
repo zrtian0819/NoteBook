242c9a2522df4d82c1293e5b9b3390a61335797a
Joe_Zhang <cjt3399@gmail.com>
2024-03-13 23:08:40 +0800

    資料放置測試與目錄添加

--- src/main.js
+++ src/main.js
@@ -1,33 +1,35 @@
 const main = document.querySelector(".MC");
+const ContList = document.querySelector(".Toc");
 
 // 異步函數的使用
+var data1 = "";
+
 async function getJson() {
   try {
     const Obj = await fetch("./123.json");
     const MyJson = await Obj.json();
+
     return MyJson;
   } catch (e) {
     console.log(e);
   }
 }
 
-let MyObj = getJson();
-
-MyObj.then(function (data) {
+getJson().then(function (data) {
   // 把連結顯示於頁面
   let i = 0;
 
-  let contents = "";
+  let toc = "";
+  for (let k = 0; k < data.length; k++) {
+    let linkTitle = data[k][0].title;
 
-  // for (let j = 1; j < data[i].length; j++) {
-  //   let caption = data[i][j].cap !== "" ? `<h2>${data[i][j].cap}</h2>` : "";
-  //   let paragraph = data[i][j].p !== "" ? `<p>${data[i][j].p}</p>` : "";
-  //   let HCode =
-  //     data[i][j].code !== ""
-  //       ? `<pre><code>${data[i][j].code}</code></pre>`
-  //       : "";
-  //   contents = contents + caption + paragraph + HCode;
-  // }
+    toc += `<li>${linkTitle}</li>`;
+
+    console.log(toc);
+  }
+  ContList.innerHTML = toc;
+
+  let contents = "";
 
   for (let j = 1; j < data[i].length; j++) {
     let tag = data[i][j].tag;
@@ -36,15 +38,13 @@ MyObj.then(function (data) {
     let class_ = data[i][j].class !== "" ? ` style=${data[i][j].class}` : "";
     let children = data[i][j].children;
 
-    // console.log(tag);
-
     if (tag == "precode") {
       let element = `<pre><code>${text}<code></pre>`;
-      console.log(element);
+      // console.log(element);
       contents += element;
     } else {
       let element = `<${tag}${style}${class_}>${text}</${tag}>`;
-      console.log(element);
+      // console.log(element);
       contents += element;
     }
   }
