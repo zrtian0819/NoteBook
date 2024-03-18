cfefa8eae41d8673b97088cf3eac696b52891435
Joe_Zhang <cjt3399@gmail.com>
2024-03-18 22:33:30 +0800

    導覽列點擊換頁功能

--- src/main.js
+++ src/main.js
@@ -1,8 +1,9 @@
 const main = document.querySelector(".MC");
 const ContList = document.querySelector(".Toc");
 
+// let ContentCode = "";
+
 // 異步函數的使用
-var data1 = "";
 
 async function getJson() {
   try {
@@ -16,49 +17,60 @@ async function getJson() {
 }
 
 getJson().then(function (data) {
-  // 把連結顯示於頁面
-  let i = 0;
-
   //匯入導覽列的清單
   let toc = "";
   for (let k = 0; k < data.length; k++) {
     let linkTitle = data[k][0].title;
 
-    toc += `<li>${linkTitle}</li>`;
-
-    console.log(toc);
+    toc += `<li class="toc_${k} point">${linkTitle}</li>`;
+    // console.log(toc);
   }
+
   ContList.innerHTML = toc;
 
-  let contents = "";
+  for (let k = 0; k < data.length; k++) {
+    t = document.querySelector(`.toc_${k}`);
+    // console.log(t);
 
-  //匯入該頁的內容
-
-  //匯入標題
-  let MajorTilte = data[i][0].title;
-  contents = `<h1 style="margin-bottom:20px; font-size:1.6rem">✅${MajorTilte}</h1>`;
-
-  //匯入內容
-  for (let j = 1; j < data[i].length; j++) {
-    let tag = data[i][j].tag;
-    let text = data[i][j].text;
-    let style = data[i][j].style !== "" ? ` style=${data[i][j].style}` : "";
-    let class_ = data[i][j].class !== "" ? ` style=${data[i][j].class}` : "";
-    // let children = data[i][j].children;
-
-    if (tag == "precode") {
-      let element = `<pre><code>${text}</code></pre>`;
-      // console.log(element);
-      contents += element;
-    } else {
-      let element = `<${tag}${style}${class_}>${text}</${tag}>`;
-      // console.log(element);
-      contents += element;
-    }
+    t.addEventListener("click", function () {
+      LoadContents(k);
+      clickNev();
+    });
   }
 
-  main.innerHTML = contents; //內容渲染
-  hljs.highlightAll(); // 將程式碼style渲染在頁面
+  let contents = "";
+  let i = 0;
+
+  function LoadContents(code) {
+    // 把連結顯示於頁面
+    let i = code;
+    //匯入該頁的內容
+    //匯入標題
+    let MajorTilte = data[i][0].title;
+    contents = `<h1 style="margin-bottom:20px; font-size:1.6rem">✅${MajorTilte}</h1>`;
+
+    //匯入內容
+    for (let j = 1; j < data[i].length; j++) {
+      let tag = data[i][j].tag;
+      let text = data[i][j].text;
+      let style = data[i][j].style !== "" ? ` style=${data[i][j].style}` : "";
+      let class_ = data[i][j].class !== "" ? ` style=${data[i][j].class}` : "";
+      // let children = data[i][j].children;
+
+      if (tag == "precode") {
+        let element = `<pre><code>${text}</code></pre>`;
+        // console.log(element);
+        contents += element;
+      } else {
+        let element = `<${tag}${style}${class_}>${text}</${tag}>`;
+        // console.log(element);
+        contents += element;
+      }
+    }
+
+    main.innerHTML = contents; //內容渲染
+    hljs.highlightAll(); // 將程式碼style渲染在頁面
+  }
 });
 
 // 按鈕的製作
@@ -68,7 +80,10 @@ const Nlist = document.querySelector(".nevbar");
 // console.log(Nlist);
 // console.log(window.innerWidth);
 
-Lbtn.addEventListener("click", function () {
+Lbtn.addEventListener("click", clickNev);
+
+function clickNev() {
+  //開關導覽頁用的
   if (Nlist.style.width === "0%") {
     if (window.innerWidth < 768) {
       Nlist.style.width = "80%";
@@ -81,4 +96,4 @@ Lbtn.addEventListener("click", function () {
     Nlist.style.width = "0%";
     Nlist.style.opacity = "0";
   }
-});
+}
